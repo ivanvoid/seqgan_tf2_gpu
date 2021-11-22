3654bd5a9280bd8cabcf12da633c58b8e89a7443
Ivan <caxaev95@gmail.com>
2021-11-22 14:01:30 +0800

    MLE stage with custum data. Test G on true eval data.

--- sequence_gan.py
+++ sequence_gan.py
@@ -18,7 +18,7 @@ EMB_DIM = 32 # embedding dimension
 HIDDEN_DIM = 32 # hidden state dimension of lstm cell
 SEQ_LENGTH = 20 # sequence length
 START_TOKEN = 0
-PRE_EPOCH_NUM = 150 # supervise (maximum likelihood estimation) epochs
+PRE_EPOCH_NUM = 20 #150 # supervise (maximum likelihood estimation) epochs
 SEED = 88
 BATCH_SIZE = 64
 
@@ -35,12 +35,11 @@ dis_batch_size = 64
 ###################################################################################
 #  Basic Training Parameters
 ###################################################################################
-TOTAL_BATCH = 200
-positive_file = 'save/real_data.txt'
-negative_file = 'save/generator_sample.txt'
-eval_file = 'save/eval_file.txt'
-generated_num = 10000
-
+TOTAL_BATCH = 10 #200
+positive_file = 'data/real_hittype.txt'
+negative_file = 'data/generator_sample.txt'
+eval_file = 'data/eval_hittype.txt'
+generated_num = 4730 #10000 # same as train_data length
 
 log_file = open('logs/train.log', 'w')
 
@@ -75,6 +74,18 @@ def target_loss(sess, target_lstm, data_loader):
     return np.mean(nll)
 
 
+def gen_eval(sess, generator, eval_loader):
+    eval_loader.reset_pointer()
+    eval_losses = []
+
+    for it in range(eval_loader.num_batch):
+        batch = eval_loader.next_batch()
+        loss = sess.run(generator.pretrain_loss, {generator.x: batch})
+        eval_losses.append(loss)
+
+    return np.mean(eval_losses)
+
+
 def pre_train_epoch(sess, trainable_model, data_loader):
     # Pre-train the generator using MLE for one epoch
     supervised_g_losses = []
@@ -93,14 +104,16 @@ def main():
     np.random.seed(SEED)
     assert START_TOKEN == 0
 
+    vocab_size = 12
+    
     gen_data_loader = Gen_Data_loader(BATCH_SIZE)
-    likelihood_data_loader = Gen_Data_loader(BATCH_SIZE) # For testing
-    vocab_size = 5000
+    eval_loader = Gen_Data_loader(BATCH_SIZE)
+    #likelihood_data_loader = Gen_Data_loader(BATCH_SIZE) # For testing
     dis_data_loader = Dis_dataloader(BATCH_SIZE)
 
     generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN)
-    target_params = pickle.load(open('save/target_params.pkl', 'rb'), encoding='latin1')
-    target_lstm = TARGET_LSTM(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN, target_params) # The oracle model
+    #target_params = pickle.load(open('save/target_params.pkl', 'rb'), encoding='latin1')
+    #target_lstm = TARGET_LSTM(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, START_TOKEN, target_params) # The oracle model
 
     discriminator = Discriminator(sequence_length=20, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim, 
                                 filter_sizes=dis_filter_sizes, num_filters=dis_num_filters, l2_reg_lambda=dis_l2_reg_lambda)
@@ -111,21 +124,29 @@ def main():
     sess.run(tf.compat.v1.global_variables_initializer())
 
     # First, use the oracle model to provide the positive examples, which are sampled from the oracle data distribution
-    generate_samples(sess, target_lstm, BATCH_SIZE, generated_num, positive_file)
+    #generate_samples(sess, target_lstm, BATCH_SIZE, generated_num, positive_file)
     gen_data_loader.create_batches(positive_file)
+    eval_loader.create_batches(eval_file)
 
     #  pre-train generator
     print('Start pre-training...')
     log('pre-training...\n')
     for epoch in range(PRE_EPOCH_NUM):
         loss = pre_train_epoch(sess, generator, gen_data_loader)
-        if epoch % 5 == 0:
-            generate_samples(sess, generator, BATCH_SIZE, generated_num, eval_file)
-            likelihood_data_loader.create_batches(eval_file)
-            test_loss = target_loss(sess, target_lstm, likelihood_data_loader)
+        buffer = '[{}/{}] Train loss {:.4f}'.format(epoch,PRE_EPOCH_NUM,loss)
+        print(buffer)
+        log(buffer)
+
+        if epoch % 5 == 0:    
+            test_loss = gen_eval(sess, generator, eval_loader)
+            #generate_samples(sess, generator, BATCH_SIZE, generated_num, eval_file)
+            #likelihood_data_loader.create_batches(eval_file)
+            #test_loss = target_loss(sess, target_lstm, likelihood_data_loader)
             print('pre-train epoch ', epoch, 'test_loss ', test_loss)
-            buffer = 'epoch:\t'+ str(epoch) + '\tnll:\t' + str(test_loss) + '\n'
-            log(buffer)
+            #buffer = 'epoch:\t'+ str(epoch) + '\tnll:\t' + str(test_loss) + '\n'
+            #log(buffer)
+
+    exit(0)
 
     print('Start pre-training discriminator...')
     # Train 3 epoch on the generated data and do this for 50 times
@@ -143,6 +164,7 @@ def main():
                 }
                 _ = sess.run(discriminator.train_op, feed)
 
+
     rollout = ROLLOUT(generator, 0.8)
 
     print('######################################################################')
@@ -157,13 +179,13 @@ def main():
             _ = sess.run(generator.g_updates, feed_dict=feed)
 
         # Test
-        if total_batch % 5 == 0 or total_batch == TOTAL_BATCH - 1:
-            generate_samples(sess, generator, BATCH_SIZE, generated_num, eval_file)
-            likelihood_data_loader.create_batches(eval_file)
-            test_loss = target_loss(sess, target_lstm, likelihood_data_loader)
-            buffer = 'epoch:\t' + str(total_batch) + '\tnll:\t' + str(test_loss) + '\n'
-            print('total_batch: ', total_batch, 'test_loss: ', test_loss)
-            log(buffer)
+        #if total_batch % 5 == 0 or total_batch == TOTAL_BATCH - 1:
+            #generate_samples(sess, generator, BATCH_SIZE, generated_num, eval_file)
+            #likelihood_data_loader.create_batches(eval_file)
+            #test_loss = target_loss(sess, target_lstm, likelihood_data_loader)
+            #buffer = 'epoch:\t' + str(total_batch) + '\tnll:\t' + str(test_loss) + '\n'
+            #print('total_batch: ', total_batch, 'test_loss: ', test_loss)
+            #log(buffer)
 
         # Update roll-out parameters
         rollout.update_params()
